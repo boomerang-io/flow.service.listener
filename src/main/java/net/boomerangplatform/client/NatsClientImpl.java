23545a0011950c4e7bafae9634148fe20f93b457
Tyson Lawrie <twlawrie@us.ibm.com>
2020-10-01 11:24:23 +1000

    Switched to STAN options builder for NATS connection

--- src/main/java/net/boomerangplatform/client/NatsClientImpl.java
+++ src/main/java/net/boomerangplatform/client/NatsClientImpl.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Service;
 import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.nats.streaming.Options;
 import io.nats.streaming.StreamingConnection;
 import io.nats.streaming.StreamingConnectionFactory;
 import net.boomerangplatform.model.Event;
@@ -20,15 +21,16 @@ public class NatsClientImpl implements NatsClient {
 
   private static final Logger LOGGER = Logger.getLogger(NatsClientImpl.class.getName());
 
+  @Value("${eventing.nats.url}")
+  private String natsUrl;
+  
   @Value("${eventing.nats.cluster}")
   private String natsCluster;
 
   @Override
   public void publishMessage(Event event, String action) {
     try {
-//      TODO do we need make the clientId unique to this running instance in case we run in HA?
-      StreamingConnectionFactory cf =
-          new StreamingConnectionFactory(natsCluster, "listener");
+      StreamingConnectionFactory cf = getStreamingConnectionFactory();
 
       EventPayload eventPayload = new EventPayload();
       eventPayload.setEvent(event);
@@ -54,9 +56,7 @@ public class NatsClientImpl implements NatsClient {
   @Override
   public void publishMessage(String subject, String jsonPayload) {
     try {
-//    TODO do we need make the clientId unique to this running instance in case we run in HA?
-      StreamingConnectionFactory cf =
-          new StreamingConnectionFactory(natsCluster, "listener");
+      StreamingConnectionFactory cf = getStreamingConnectionFactory();
 
       try (StreamingConnection sc = cf.createConnection()) {
         sc.publish(subject, jsonPayload.getBytes(StandardCharsets.UTF_8));
@@ -70,5 +70,13 @@ public class NatsClientImpl implements NatsClient {
     }
 //    Do we need a finally with sc.close()
 
+  }
+
+  private StreamingConnectionFactory getStreamingConnectionFactory() {
+//  TODO do we need make the clientId unique to this running instance in case we run in HA?
+    Options cfOptions = new Options.Builder().natsUrl(natsUrl).clusterId(natsCluster).clientId("listener").build();
+    StreamingConnectionFactory cf =
+        new StreamingConnectionFactory(cfOptions);
+    return cf;
   }
 }
