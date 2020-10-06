1af242153f56a2301efb58fdc97e71f4a2903135
Iulian Corcoja <iulian@corcoja.com>
2020-10-06 19:37:21 +0300

    Formatter for event controller

--- src/main/java/net/boomerangplatform/service/EventProcessorImpl.java
+++ src/main/java/net/boomerangplatform/service/EventProcessorImpl.java
@@ -23,124 +23,113 @@ import net.boomerangplatform.client.WorkflowClient;
 @Service
 public class EventProcessorImpl implements EventProcessor {
 
-  private static final Logger logger = LogManager.getLogger(EventProcessorImpl.class);
+    private static final Logger logger = LogManager.getLogger(EventProcessorImpl.class);
 
-  protected static final String TYPE_PREFIX = "io.boomerang.eventing.";
-  
-  protected static final String SUBJECT = "flow-workflow-execute";
-  
-  @Value("${eventing.enabled}")
-  private Boolean eventingEnabled;
+    private static final String TYPE_PREFIX = "io.boomerang.eventing.";
 
-  @Value("${eventing.auth.enabled}")
-  private Boolean authzEnabled;
+    private static final String SUBJECT = "flow-workflow-execute";
 
-  @Autowired
-  private NatsClient natsClient;
-  
-  @Autowired
-  private WorkflowClient workflowClient;
+    @Value("${eventing.enabled}")
+    private Boolean eventingEnabled;
 
-  @Override
-  public HttpStatus routeEvent(String token, String requestUri, String trigger, String workflowId, JsonNode payload) {
-    final String eventId = UUID.randomUUID().toString();
-    final String eventType = TYPE_PREFIX + trigger;
-    final URI uri = URI.create(requestUri);
-    final String subject = "/" + workflowId;
-    
-    if (!checkAccess(workflowId, trigger, token)) {
-      return HttpStatus.FORBIDDEN;
+    @Value("${eventing.auth.enabled}")
+    private Boolean authzEnabled;
+
+    @Autowired
+    private NatsClient natsClient;
+
+    @Autowired
+    private WorkflowClient workflowClient;
+
+    @Override
+    public HttpStatus routeEvent(String token, String requestUri, String trigger, String workflowId, JsonNode payload) {
+        final String eventId = UUID.randomUUID().toString();
+        final String eventType = TYPE_PREFIX + trigger;
+        final URI uri = URI.create(requestUri);
+        final String subject = "/" + workflowId;
+
+        if (!checkAccess(workflowId, trigger, token)) {
+            return HttpStatus.FORBIDDEN;
+        }
+
+        final CloudEventImpl<JsonNode> cloudEvent = CloudEventBuilder.<JsonNode>builder().withType(eventType)
+                .withId(eventId).withSource(uri).withData(payload).withSubject(subject).withTime(ZonedDateTime.now())
+                .build();
+
+        final String jsonPayload = Json.encode(cloudEvent);
+        logger.info("CloudEvent Object - " + jsonPayload);
+
+        if (eventingEnabled) {
+            natsClient.publish(eventId, SUBJECT, jsonPayload);
+        } else {
+            workflowClient.executeWorkflowPut(SUBJECT, cloudEvent);
+        }
+
+        return HttpStatus.OK;
     }
-        
-    final CloudEventImpl<JsonNode> cloudEvent =
-    CloudEventBuilder.<JsonNode>builder()
-      .withType(eventType)
-      .withId(eventId)
-      .withSource(uri)
-      .withData(payload)
-      .withSubject(subject)
-      .withTime(ZonedDateTime.now())
-      .build();
 
-    final String jsonPayload = Json.encode(cloudEvent);
-    logger.info("CloudEvent Object - " + jsonPayload);
-    if (eventingEnabled) {
-      natsClient.publish(eventId, SUBJECT, jsonPayload);
-    } else {
-      workflowClient.executeWorkflowPut(SUBJECT, cloudEvent);
-    }
-    
-    return HttpStatus.OK;
-  }
-  
-  @Override
-  public HttpStatus routeCloudEvent(String token, String requestUri, Map<String, Object> headers, JsonNode payload) {
+    @Override
+    public HttpStatus routeCloudEvent(String token, String requestUri, Map<String, Object> headers, JsonNode payload) {
 
-    logger.info("routeCloudEvent() - Event as Message String: " + payload.toString());
-    
-    CloudEvent<AttributesImpl, JsonNode> event = Unmarshallers.structured(JsonNode.class)
-        .withHeaders(() -> headers).withPayload(() -> payload.toString()).unmarshal();
+        logger.info("routeCloudEvent() - Event as Message String: " + payload.toString());
 
-    logger.info("routeCloudEvent() - Attributes: " + event.getAttributes().toString());
-    JsonNode eventData = event.getData().get();
-    logger.info("routeCloudEvent() - Data: " + eventData.toPrettyString());
+        CloudEvent<AttributesImpl, JsonNode> event = Unmarshallers.structured(JsonNode.class).withHeaders(() -> headers)
+                .withPayload(() -> payload.toString()).unmarshal();
 
-    final String eventId = UUID.randomUUID().toString();
-    final String eventType = TYPE_PREFIX + "custom";
-    final URI uri = URI.create(requestUri);
-    String subject = event.getAttributes().getSubject().orElse("");
-    if (!subject.startsWith("/")) {
-      return HttpStatus.BAD_REQUEST;
-    }
-    
-    if (!checkAccess(getWorkflowIdFromSubject(subject), "custom", token)) {
-      return HttpStatus.FORBIDDEN;
-    }
-    
-    final CloudEventImpl<JsonNode> cloudEvent =
-    CloudEventBuilder.<JsonNode>builder()
-      .withType(eventType)
-      .withId(eventId)
-      .withSource(uri)
-      .withData(payload)
-      .withSubject(subject)
-      .withTime(ZonedDateTime.now())
-      .build();
+        logger.info("routeCloudEvent() - Attributes: " + event.getAttributes().toString());
+        JsonNode eventData = event.getData().get();
+        logger.info("routeCloudEvent() - Data: " + eventData.toPrettyString());
 
-    final String jsonPayload = Json.encode(cloudEvent);
-    logger.info("CloudEvent Object - " + jsonPayload);
-    if (eventingEnabled) {
-      natsClient.publish(eventId, SUBJECT, jsonPayload);
-    } else {
-      workflowClient.executeWorkflowPut(SUBJECT, cloudEvent);
-    }
-    
-    return HttpStatus.OK;
-  }
-  
-  //TODO replace with SecurityConfig and SecurityFilter
-  private Boolean checkAccess(String workflowId, String trigger, String token) {
-    if (authzEnabled) {
-      logger.info("checkAccess() - Token: " + token);
-      if (token != null) {
-        return workflowClient.validateTriggerToken(workflowId, trigger, token);
-      } else {
-        logger.error("checkAccess() - Error: no token provided.");
-        return false;
-      }
-    } else {
-      return true;
-    }
-  }
+        final String eventId = UUID.randomUUID().toString();
+        final String eventType = TYPE_PREFIX + "custom";
+        final URI uri = URI.create(requestUri);
+        String subject = event.getAttributes().getSubject().orElse("");
+        if (!subject.startsWith("/")) {
+            return HttpStatus.BAD_REQUEST;
+        }
 
-  private String getWorkflowIdFromSubject(String subject) {
-    // Reference 0 will be an empty string as it is the left hand side of the split
-    String[] splitArr = subject.split("/");
-    if (splitArr.length >= 2) {
-      return splitArr[1].toString();
-    } else {
-      logger.error("processCloudEvent() - Error: No workflow ID found in event");
-      return "";
+        if (!checkAccess(getWorkflowIdFromSubject(subject), "custom", token)) {
+            return HttpStatus.FORBIDDEN;
+        }
+
+        final CloudEventImpl<JsonNode> cloudEvent = CloudEventBuilder.<JsonNode>builder().withType(eventType)
+                .withId(eventId).withSource(uri).withData(payload).withSubject(subject).withTime(ZonedDateTime.now())
+                .build();
+
+        final String jsonPayload = Json.encode(cloudEvent);
+        logger.info("CloudEvent Object - " + jsonPayload);
+        if (eventingEnabled) {
+            natsClient.publish(eventId, SUBJECT, jsonPayload);
+        } else {
+            workflowClient.executeWorkflowPut(SUBJECT, cloudEvent);
+        }
+
+        return HttpStatus.OK;
     }
-  }
+
+    // TODO replace with SecurityConfig and SecurityFilter
+    private Boolean checkAccess(String workflowId, String trigger, String token) {
+        if (authzEnabled) {
+            logger.info("checkAccess() - Token: " + token);
+            if (token != null) {
+                return workflowClient.validateTriggerToken(workflowId, trigger, token);
+            } else {
+                logger.error("checkAccess() - Error: no token provided.");
+                return false;
+            }
+        } else {
+            return true;
+        }
+    }
+
+    private String getWorkflowIdFromSubject(String subject) {
+        // Reference 0 will be an empty string as it is the left hand side of the split
+        String[] splitArr = subject.split("/");
+        if (splitArr.length >= 2) {
+            return splitArr[1].toString();
+        } else {
+            logger.error("processCloudEvent() - Error: No workflow ID found in event");
+            return "";
+        }
+    }
 }
