d51edda54ccb2502f38f8f55129a17b47471cebb
Thamada Durga Sri <Thamada.Durga.Sri@ibm.com>
2026-09-11 19:32:02 +0530

    fix: upgraded springboot

--- src/main/java/io/boomerang/Application.java
+++ src/main/java/io/boomerang/Application.java
@@ -2,14 +2,13 @@ package io.boomerang;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableAsync;
 
 import io.swagger.v3.oas.models.OpenAPI;
 
 @EnableAsync(proxyTargetClass = true)
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 public class Application {
 
   public static void main(String[] args) {
